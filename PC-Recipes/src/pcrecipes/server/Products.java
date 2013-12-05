ec98e366edf5e445f3754f5cf0a164d447a04190
liorzaken <zakenlior@gmail.com>
2013-12-05 17:29:45 +0200

    ניסיון לשימוש ב datastore

--- PC-Recipes/src/pcrecipes/server/Products.java
+++ PC-Recipes/src/pcrecipes/server/Products.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Products {
 
-	private String naimProd;
+	private String nameProd;
 	private int idProd=0;
 	private String swapProd;
 	//public int i=0;
@@ -12,14 +12,14 @@ public class Products {
 	
 	/////////constructor without swap option
 	public Products(String name){
-		naimProd=name;
+		nameProd=name;
 		idProd++;
 		swapProd="not found";
 
 	}
 	/////////constructor with swap option
 	public Products(String name, String swap){
-		naimProd=name;
+		nameProd=name;
 		idProd++;
 		swapProd=swap;
 	}
@@ -27,10 +27,10 @@ public class Products {
 
 	public String getNaimProd()
 	{
-		return naimProd;
+		return nameProd;
 	}
 	public void setNaameProd(String nameProd){
-		this.naimProd=nameProd;
+		this.nameProd=nameProd;
 	}
 
 	public int getIdProd()
@@ -51,7 +51,7 @@ public class Products {
 
 	public String toString()
 	{
-		return (naimProd +"שם המוצר"+"\n" + idProd +"מספר סידורי"+"\n"+ swapProd+"תחליף" +"\n");
+		return (nameProd +"שם המוצר"+"\n" + idProd +"מספר סידורי"+"\n"+ swapProd+"תחליף" +"\n");
 	}
 }
 
--- PC-Recipes/src/pcrecipes/servlet/SearchServlet.java
+++ PC-Recipes/src/pcrecipes/servlet/SearchServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import pcrecipes.server.recipe;
 import pcrecipes.server.recipes;
@@ -63,5 +64,8 @@ public class SearchServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Object name = request.getParameter("SearchItem");
+		System.out.println("The Name Is:"+name.toString());
+		response.sendRedirect("recipePage");
 	}
 }
